b1abc19feb8072196be2d98ab7c6dd9a5c58ce5d
bixu <bixv@icloud.com>
2014-02-18 16:24:05 +0100

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 default["manta"]["user"] = "root"
-default["manta"]["install_path"] = "/"
+default["manta"]["install_path"] = "/root"
 default["manta"]["endpoint"] = "https://us-east.manta.joyent.com"
