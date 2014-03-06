e87d5bcb66d93d981699f4c4c81605bed388dfcc
Eric Saxby & James Hart <pair+sax+james@wanelo.com>
2014-03-06 13:25:16 -0800

    Remove ssh-agent ppid file if agent is not running

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "ops@wanelo.com"
 license          "Apache 2.0"
 description      "Installs/Configures manta"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.3"
+version          "1.0.4"
 
 depends "nodejs"
 depends "npm"
