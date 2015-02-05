7e92b3997c16dc490e7c22e6e2ab5401ce072944
Eric Saxby <sax@livinginthepast.org>
2015-02-05 10:19:46 -0800

    Use MIT license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "manta"
 maintainer       "Wanelo, Inc"
 maintainer_email "ops@wanelo.com"
-license          "Apache 2.0"
+license          "MIT"
 description      "Installs/Configures manta"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.4"
