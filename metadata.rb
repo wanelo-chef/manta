766fb2229656d81ae32783cc6f7fa055ad6e0ea5
Eric Saxby <sax@livinginthepast.org>
2015-08-12 15:30:16 -0700

    Use ssh-keygen from /usr/bin Some newer version of ssh-keygen does not give fingerprints with -l -f

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "ops@wanelo.com"
 license          "MIT"
 description      "Installs/Configures manta"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.4"
+version          "1.0.5"
 
 depends "nodejs"
 depends "npm"
